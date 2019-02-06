202490a0cafca20358be74a33d73a09141440ee5
Sam <sam.nguyen.1223@gmail.com>
2019-02-06 16:52:51 -0500

    Added more content

--- CompanyPage/Company.js
+++ CompanyPage/Company.js
@@ -1,3 +1,36 @@
 $(window).scroll(function() {
+    var wScroll = $(this).scrollTop();
 
+    if (wScroll > $(".headShot").offset().top - ($(window).height() / 1.2)) {
+        $(".headShot").each(function(i) {
+            setTimeout(function() {
+                $(".headShot").eq(i).addClass("isShowing")
+            }, (150 * (i + 1)));
+        });
+
+    }
+});
+
+$("#navTitle").click(function() {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 400);
+});
+
+$("#navOne").click(function() {
+    $("html, body").animate({
+        scrollTop: $("#one").offset().top - 80
+    }, 400);
+});
+
+$("#navTwo").click(function() {
+    $("html, body").animate({
+        scrollTop: $("#two").offset().top - 80
+    }, 400);
+});
+
+$("#navThree").click(function() {
+    $("html, body").animate({
+        scrollTop: $("#three").offset().top - 80
+    }, 400);
 });
