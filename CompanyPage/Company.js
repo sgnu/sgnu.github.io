8fe613eebc11548c53a9c6a2bcfdc2dac644f799
Sam <sam.nguyen.1223@gmail.com>
2019-02-06 18:09:00 -0500

    Added some support for unwide devices

--- CompanyPage/Company.js
+++ CompanyPage/Company.js
@@ -17,6 +17,16 @@ $("#navTitle").click(function() {
     }, 400);
 });
 
+$("#nav i").click(function() {
+    $("#nav a").toggle();
+
+    if ($("#nav").is("expanded")) {
+        $("#nav").removeClass("expanded");
+    } else {
+        $("#nav").addClass("expanded");
+    }
+})
+
 $("#navOne").click(function() {
     $("html, body").animate({
         scrollTop: $("#one").offset().top - 80
